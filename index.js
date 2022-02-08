957ab88b40fe0c06a5d454b43b445e0bf5e0f286
Irfan Rosly <irfanrosly@gmail.com>
2022-02-08 14:45:36 +0800

    Custom Image Caching

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-/**
- * @format
- */
+import React from 'react';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import FastImage from 'react-native-fast-image'; //install from MBB fork with D++ features ;)
+import PropTypes from 'prop-types';
 
-AppRegistry.registerComponent(appName, () => App);
+//i need to allow any props existing comes
+export const CustomFastImage = props => {
+  let {
+    source, // already
+    resizeMode, //already
+    style, //already
+    name, //already ???
+    key, //already ???
+    priority, // new
+    headers, // new
+  } = props;
+
+  if (!source?.priority && source.uri) {
+    source = {
+      ...source,
+      ...(headers && {headers: headers}),
+      ...(priority && {priority: 'high'}),
+    };
+  }
+
+  // facade pattern
+
+  return (
+    <FastImage
+      // key={key}
+      // name={name}
+      style={{...style}}
+      source={source}
+      {...props}
+      resizeMode={resizeMode}
+      //   onLoad={onImageLoad}
+      // onError={onError}
+    />
+  );
+};
+
+CustomFastImage.propTypes = {
+  source: PropTypes.any.isRequired,
+  name: PropTypes.string,
+  key: PropTypes.string,
+
+  priority: PropTypes.oneOf(['low', 'normal', 'high']),
+  headers: PropTypes.any,
+
+  resizeMode: PropTypes.oneOf(['contain', 'cover', 'stretch', 'center']),
+};
+
+CustomFastImage.defaultProps = {
+  priority: 'high',
+};
